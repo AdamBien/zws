5fb1852a72703e6f6956758bd1c5672413e2ded8
Adam Bien <abien@adam-bien.com>
2025-09-24 05:36:16 +0200

    custom header added for caching prevention

--- ZWS.java
+++ ZWS.java
@@ -1,22 +1,38 @@
 import java.awt.Desktop;
 
-import com.sun.net.httpserver.SimpleFileServer;
+import module jdk.httpserver;
 import com.sun.net.httpserver.SimpleFileServer.OutputLevel;
+
 
 void main(String... args) throws IOException, InterruptedException {
     var port = 3000;
     var loopback = new InetSocketAddress(InetAddress.getLoopbackAddress(), port);
     var directory = args.length > 0 ? args[0] : ".";
     var path = Path.of(directory).normalize().toAbsolutePath();
-    var webServer = SimpleFileServer.createFileServer(loopback, path, OutputLevel.VERBOSE);
-    webServer.start();
+
+    var server = HttpServer.create(loopback, 0);
+    var handler = SimpleFileServer.createFileHandler(path);
+    server.createContext("/", new NoCacheHandler(handler));
+    server.start();
+
     var url = "http://%s:%d".formatted(
-            webServer.getAddress().getHostString(),
-            webServer.getAddress().getPort());
+            server.getAddress().getHostString(),
+            server.getAddress().getPort());
     IO.println("Serving files from: " + path);
     IO.println(url);
     Browser.open(url);
     IO.println("browser opened ");
+}
+
+record NoCacheHandler(HttpHandler handler) implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        var headers = exchange.getResponseHeaders();
+        headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        headers.add("Pragma", "no-cache");
+        headers.add("Expires", "0");
+        handler.handle(exchange);
+    }
 }
 
 interface Browser {
